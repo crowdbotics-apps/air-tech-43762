53ab5870aa6c434fee9fa3eb5767f9d7fa96e607
Crowdbotics <team@crowdbotics.com>
2023-10-04 23:11:39 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "air-tech-43762",
+  url: "https://air-tech-43762.botics.co",
+  api: "https://air-tech-43762.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
